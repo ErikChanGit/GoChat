79a69bef8a16c82a611cbaed6a89072dd5d90fb4
ErikChan <1043490933@qq.com>
2022-01-04 16:34:05 +0800

    UpdateAction7

--- tools/new_tag.py
+++ tools/new_tag.py
@@ -1,5 +1,5 @@
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.check_call('git tag -a v0.1.6 -m "Release_v0.1.6"')
-    subprocess.check_call('git push origin v0.1.6')
+    subprocess.check_call('git tag -a v0.1.7 -m "Release_v0.1.7"')
+    subprocess.check_call('git push origin v0.1.7')
