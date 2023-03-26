759c44a45ff7ac2c793110800bc7bbdc54af62f2
ErikChan <1043490933@qq.com>
2023-03-26 12:36:52 +0800

    update

--- tools/new_tag.py
+++ tools/new_tag.py
@@ -1,5 +1,20 @@
 import subprocess
+# git config --global user.email "1043490933@qq.com"
+#             git config --global user.name "ErikChanGit"
+#             git tag -a v0.1.8 -m "Release_v0.1.8"
+#             git push origin v0.1.8
+
+
+# cd ${{ github.workspace }}/tools
+#             python new_tag.py
+
+def clone_from_git(repository, bsp_repository_dest):
+    ''' 将 github 的源码克隆到本地 '''
+    if os.path.exists(bsp_repository_dest + "/.git") is False:
+        subprocess.check_call("git clone " + repository + " " + bsp_repository_dest)
+
 
 if __name__ == '__main__':
-    subprocess.check_call('git tag -a v0.1.7 -m "Release_v0.1.7"')
-    subprocess.check_call('git push origin v0.1.7')
+    clone_from_git("git@github.com:ErikChanGit/GoChat.git", "/sdk/GoChat")
+    subprocess.check_call('git tag -a v0.1.8 -m "Release_v0.1.8"', cwd="/sdk/GoChat")
+    subprocess.check_call('git push origin v0.1.8', cwd="/sdk/GoChat")
